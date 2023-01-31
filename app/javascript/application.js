9f7847637e403c35ec739854174eb0a5a227889d
Kurnia Muhamad <omenkzz@gmail.com>
2023-01-31 11:28:25 +0700

    Update product list using props

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ProductList from './components/ProductList';
+import App from './components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<ProductList />);
+  root.render(<App />);
 });
