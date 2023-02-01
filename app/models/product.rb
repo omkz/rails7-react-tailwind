873b7680edc101966bd585789b76dc3591ca13cd
Kurnia Muhamad <omenkzz@gmail.com>
2023-02-02 03:00:01 +0700

    Linking user and product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
+  belongs_to :user
+
   validates :price, presence: true
   validates :title, presence: true
-  
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-Product.create([{ title: "title 1", price: 7.99}, { title: "title 2", price: 7.77}])
+user = User.create(email: 'user@email.com', password: 'password', password_confirmation: 'password')
+user.products.create([{ title: "title 1", price: 7.99}, { title: "title 2", price: 7.77}])
+
