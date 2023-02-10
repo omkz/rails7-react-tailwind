a2a4d079a1f4496273628e0142f63467e65984c0
Kurnia Muhamad <omenkzz@gmail.com>
2023-02-10 18:19:07 +0700

    Refactor Product in the Home Page

--- app/javascript/features/products/components/product-item.js
+++ app/javascript/features/products/components/product-item.js
@@ -2,15 +2,20 @@ import React from 'react';
 import {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 
-const ProductItem = ({item, onRemove}) => {
+const ProductItem = ({item, onRemove, onUpdate}) => {
     return (
         <li>
             <span>{item.title}</span>
             <span>{item.price}</span>
+
+            {onRemove ?
             <button type="button" onClick={() => onRemove(item.id)}>
-                Remove
-            </button>
-            <Link to={`/products/edit/${item.id}`} className="button is-small is-info">Edit</Link>
+                | Remove
+            </button> : "" }
+
+            {onUpdate ?
+            <Link to={`/products/edit/${item.id}`} className="button is-small is-info"> Edit</Link> : "" }
+
         </li>
     )
 }
--- app/javascript/pages/home.js
+++ app/javascript/pages/home.js
@@ -1,12 +1,24 @@
-import React from "react";
-import ProductHome from "../component/product-home";
+import React, {useEffect, useState} from "react";
+import ProductList from "../features/products/components/product-list";
 
 
 const Home = () => {
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const response = await fetch('/api/v1/products.json');
+        const data = await response.json();
+        setList(data);
+    }
+
     return (
         <div>
             <h2>This is Home Page</h2>
-            <ProductHome />
+            <ProductList list={list} onRemove={false} onUpdate={false}/>
         </div>
     )
 }
