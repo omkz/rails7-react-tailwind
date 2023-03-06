a150dab26dbba015347826a75906d25d3786bbd2
Kurnia Muhamad <omenkzz@gmail.com>
2023-03-07 04:13:08 +0700

    Update my products page style

--- app/javascript/features/products/components/product-item.js
+++ app/javascript/features/products/components/product-item.js
@@ -3,20 +3,26 @@ import {Link} from "react-router-dom";
 
 const ProductItem = ({item, onRemove, onUpdate}) => {
     return (
-        <li>
-            <span>{item.attributes.title}</span>
-            <span>{item.attributes.price}</span>
-            <span> <img src={item.attributes.image_url} alt="product image" className="latest-image" /></span>
+        <div className="grid grid-cols-6 items-center gap-4">
+            <div className="mb-4">
+                <img src={item.attributes.image_url} alt="product image" className="latest-image"/>
+            </div>
+            <div className="mb-4">
+                {item.attributes.title}
+            </div>
+            <div className="mb-4">
+                {item.attributes.price}
+            </div>
+            <div className="mb-4">
+                {onRemove ?
+                    <button type="button" onClick={() => onRemove(item.id)}>
+                        Remove | 
+                    </button> : ""}
 
-            {onRemove ?
-            <button type="button" onClick={() => onRemove(item.id)}>
-                | Remove
-            </button> : "" }
-
-            {onUpdate ?
-            <Link to={`/products/edit/${item.id}`} className="button is-small is-info"> Edit</Link> : "" }
-
-        </li>
+                {onUpdate ?
+                    <Link to={`/products/edit/${item.id}`} className="button is-small is-info"> Edit</Link> : ""}
+            </div>
+        </div>
     )
 }
 
